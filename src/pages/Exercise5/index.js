ad55e35ca3797df6258048c914cef9f3c843aacf
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2023-04-16 20:25:37 -0500

    add function for the argument requirement

--- src/pages/Exercise5/index.js
+++ src/pages/Exercise5/index.js
@@ -10,24 +10,27 @@ const Exercise5 = () => {
       const tempValue = prompt("Increse el dato " + i)
       data.push(+tempValue);
     };
-
     // the number tha the user wnats to analize
     const numberToAnalize = prompt("Ingrese el dato a calcular")
 
+    analyzeNumber(data, numberToAnalize)
+  };
+
+  const analyzeNumber = (arrayToAnalyze, numberToAnalyze) => {
     // Accumulator to know how many times de element is repeated
     let timesNumberIsRepeated = 0;
 
     // Loop to pass over eache element in the array
-    for(let i = 0; i < data.length; i++) {
+    for(let i = 0; i < arrayToAnalyze.length; i++) {
       // condition to realize if the element is equl to the current element in the array
-      if(data[i] === +numberToAnalize) {
+      if(arrayToAnalyze[i] === +numberToAnalyze) {
         timesNumberIsRepeated += 1;
       }
     }
 
     // output
-    alert('El dato ' + numberToAnalize + ' se repite ' + timesNumberIsRepeated);
-  };
+    alert('El dato ' + numberToAnalyze + ' se repite ' + timesNumberIsRepeated);
+  }
 
   return (
     <div>
